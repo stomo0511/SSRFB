4572ed3015b0fcdcaffb786646a1b5bb5a64fe82
stomo <stomo@yamanashi.ac.jp>
2018-06-04 17:47:17 +0900

    temp

--- SSRFB.cpp
+++ SSRFB.cpp
@@ -10,12 +10,13 @@
 #include <cfloat>
 #include <cmath>
 #include <cassert>
+#include <sys/time.h>
 
 #include <CoreBlasTile.hpp>
 #include <TMatrix.hpp>
 
 #define MAX_DIVS 100     // Maximum number of candidates of inner block size
-#define MAX_THREADS 1
+#define MAX_THREADS 8
 #define TRIES 5          // # of measurement
 
 using namespace std;
@@ -60,124 +61,63 @@ int main(int argc, const char * argv[])
     ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////
-    // measure minimum number of measurement for one inner block size
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    double min = DBL_MAX;
-    int cc;
-
-    for (int i = 0; i<count; i++)
-    {
-        if ((ibs[i] >= 10) && (ibs[i] != nb))
-        {
-        	double tmin = DBL_MAX;
-
-            TMatrix A( 2*nb, (MAX_THREADS+1)*nb, nb, nb, ibs[i]);
-            TMatrix T( 2*ibs[i], (MAX_THREADS+1)*nb, ibs[i], nb, ibs[i]);
-
-            A.Set_Rnd(20180531);
-
-			#pragma omp parallel
-            {
-				#pragma omp single
-            	{
-            		GEQRT( A(0,0), T(0,0) );
-            	}
-
-				#pragma omp for
-            	for (int j=1; j<=MAX_THREADS; j++)
-            	{
-            		LARFB( PlasmaLeft, PlasmaTrans, A(0,0), T(0,0), A(0,j) );
-            	}
-
-				#pragma omp single
-            	{
-            		TSQRT( A(0,0), A(1,0), T(1,0) );
-            	}
-
-            	// Timer start
-            	double ttime = omp_get_wtime();
-
-				#pragma omp for
-            	for (int j=1; j<=MAX_THREADS; j++)
-            	{
-            		SSRFB( PlasmaLeft, PlasmaTrans, A(1,0), T(1,0), A(0,j), A(1,j) );
-            	}
-
-            	// Timer stop
-            	ttime = omp_get_wtime() - ttime;
-
-				#pragma omp critical
-            	{
-            		if (tmin > ttime)
-            			tmin = ttime;
-            	}
-				#pragma omp barrier
-            } // End of parallel section
-
-            if (tmin < min)
-            	min = tmin;
-
-        }  // if (ibs[i] >= 0) END
-    }  // i-loop END
-
-    cc = (int)(ceil(wtick / min));
-    #ifdef DEBUG
-    cout << "cc = " << cc << endl;
-	#endif
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     double time;
 
-    for (int c = 0; c<TRIES; c++)
+    for (int c = 0; c<TRIES+1; c++)
     {
         for (int i = 0; i<count; i++)
         {
             if ((ibs[i] >= 10) && (ibs[i] != nb))
             {
-            	cout << nb << "," << ibs[i] << "," << flush;
+				#pragma omp master
+            	time = DBL_MAX;
 
                 TMatrix A( 2*nb, (MAX_THREADS+1)*nb, nb, nb, ibs[i]);
                 TMatrix T( 2*ibs[i], (MAX_THREADS+1)*nb, ibs[i], nb, ibs[i]);
 
                 A.Set_Rnd(20180423);
 
-//				#pragma omp parallel private(time)
+				#pragma omp parallel
                 {
-//					#pragma omp single
+                	double ttime;
+
+					#pragma omp single
                 	{
                 		GEQRT( A(0,0), T(0,0) );
                 	}
 
-//					#pragma omp for
+					#pragma omp for
                 	for (int j=1; j<=MAX_THREADS; j++)
                 	{
                 		LARFB( PlasmaLeft, PlasmaTrans, A(0,0), T(0,0), A(0,j) );
                 	}
 
-//					#pragma omp single
+					#pragma omp single
                 	{
                 		TSQRT( A(0,0), A(1,0), T(1,0) );
                 	}
 
                 	// Timer start
-                	time = omp_get_wtime();
+                	ttime = omp_get_wtime();
 
-//					#pragma omp for
-                	for (int j=1; j<=MAX_THREADS*cc; j++)
-//                	for (int j=1; j<=MAX_THREADS; j++)
+					#pragma omp for schedule(static,1)
+                	for (int j=1; j<=MAX_THREADS; j++)
                 	{
-                		SSRFB( PlasmaLeft, PlasmaTrans, A(1,0), T(1,0), A(0,j%MAX_THREADS), A(1,j%MAX_THREADS) );
-//                		SSRFB( PlasmaLeft, PlasmaTrans, A(1,0), T(1,0), A(0,j), A(1,j) );
+                		SSRFB( PlasmaLeft, PlasmaTrans, A(1,0), T(1,0), A(0,j), A(1,j) );
                 	}
 
                 	// Timer stop
-                	time = omp_get_wtime() - time;
-                	time /= cc;
+                	ttime = omp_get_wtime() - ttime;
 
-					#pragma omp master
-                		cout << time << endl;
+					#pragma omp critical
+                	{
+                		if (time > ttime)
+                			time = ttime;
+                	}
                 }
+                if (c != 0)
+                	cout << nb << "," << ibs[i] << "," << time << endl;
             }  // if (ibs[i] >= 0) END
         }  // i-loop END
     }   // c-loop END
